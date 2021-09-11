533851fd4848b72eb9d9d2d1d462350e2827aac8
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-09-11 14:17:35 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JYWZIIQYqqI4EiS0LZwRXNDH9tCmzQsoPqiGv1VqMF9ObJEln9SNHXzHmJRAn9NgB8YxH6LUVyMvwtC9lYvuB9c006j3p976c',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
