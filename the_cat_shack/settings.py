81a37913e8fc7fd61bf881ecc05df441c2353210
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-09-21 12:55:48 +0000

    Deployment part 1 setting up heroku and database

--- the_cat_shack/settings.py
+++ the_cat_shack/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,12 +122,12 @@ WSGI_APPLICATION = 'the_cat_shack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+ DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
 
 
 # Password validation
