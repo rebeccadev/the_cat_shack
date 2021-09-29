3c7dba2ae186310c0940f182ff8763cf3c78f965
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-09-29 17:17:15 +0000

    Fix python errors with pep8 validator for bag and blog apps

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from .forms import CommentForm
 from django.shortcuts import render, get_object_or_404
 
+
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/blog.html'
